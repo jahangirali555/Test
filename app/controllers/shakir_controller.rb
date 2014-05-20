30fa1e4c6415fda908dae0c08fcfc899ad1f54e1
jahangirali555 <jahangirali555@gmail.com>
2014-05-20 07:51:52 +0000

    First project on ruby on rails

---
+++ app/controllers/shakir_controller.rb
@@ -0,0 +1,17 @@
+class ShakirController < ApplicationController
+  def index
+  	@title = "Main Index Page"
+  end
+
+  def events
+  	@title = "Events"
+  end
+
+  def shop
+  	@title = "Shop"
+  end
+
+  def discussion
+  	@title = "Discussion"
+  end
+end
---
+++ test/helpers/shakir_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class ShakirHelperTest < ActionView::TestCase
+end
